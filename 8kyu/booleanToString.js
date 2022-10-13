e2c8e6e1c27a4b82070e65b5833a27011ecac42e
yamamoto-yuka <Yuka.Yamamoto@vanarts.com>
2022-10-13 14:29:49 -0700

    Created bool to string file

---
+++ 8kyu/booleanToString.js
@@ -0,0 +1,13 @@
+function boolToWord(bool){
+ if(bool) return 'Yes';
+ else return 'No';
+}
+
+// Shorthand code
+function boolToWord2(bool){
+  return bool ? 'Yes' : 'No';
+}
+
+let boolToWord3 = (bool) =>{
+  return bool ? 'Yes' :'No';
+}
